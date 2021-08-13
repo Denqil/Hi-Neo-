4c49b6036378f723373aed862dc4803c95a05836
Denis <Denis@DESKTOP-P4038IK>
2021-08-13 21:06:59 +0700

    Добавьте файлы проекта.

---
+++ wirk1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace wirk1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Работа с консолью";
+
+            Console.WindowHeight = Console.LargestWindowHeight;
+            Console.WindowWidth = Console.LargestWindowWidth;
+
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+
+            Console.WriteLine("Wake up, Neo...");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("The Matrix has you...");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Follow the white rabbit.");
+            Console.ReadKey();
+            Console.Clear();
+
+
+            Console.WriteLine("Knock, knock, Neo.");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.ReadLine();
+        }
+    }
+}
